787dd02cc853f611fa0938e2443c9b391a4383ab
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-15 08:51:24 -0700

    Hello, puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program to print an actual sentence
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
