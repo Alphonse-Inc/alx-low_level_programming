7b9a839ee6b5effba39290d466dd3dffe91037c3
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-16 17:12:05 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Input
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	char alp2;
+
+	alp = 'a';
+	alp2 = 'A';
+	while
+		(alp <= 'z') {
+			putchar (alp);
+			alp++;
+		}
+	while
+		(alp2 <= 'z') {
+                        putchar (alp2);
+                        alp2++;
+		}
+	putchar('\n');
+	return (0);
+}
