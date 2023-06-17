a5ae6d467e83990dcaef2813e02fa94de20b1d67
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-16 18:13:59 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ *main - Input
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int dig = 0;
+
+	while (dig <= 9)
+	{
+		putchar(dig + '0');
+		dig++;
+	}
+	printf("\n");
+
+	return (0);
+}
