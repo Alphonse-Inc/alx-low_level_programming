bee1a1c312d40df8562e615e7d215fc2c1f96888
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-19 06:09:54 -0700

    Patience task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Input
+ * Patience task
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + 48);
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
