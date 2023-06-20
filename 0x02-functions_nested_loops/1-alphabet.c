a5541e7aa4146de91ecc3831e2444187d29ea992
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-20 06:47:56 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Input
+ * Task 1
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
