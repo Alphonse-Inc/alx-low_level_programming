e244344b62cc590116f0a27edfce6b92173e7e23
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-23 17:53:53 -0700

    collaboration is multiplication task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - This should multiply two numbers
+ * @x: This is the number to be multiplied
+ * @y: This is the second number to be multiplied
+ * Return: Answer to the multiplication of x and y
+ */
+
+int mul(int x, int y)
+        
+{       
+        return (x * y);
+}
