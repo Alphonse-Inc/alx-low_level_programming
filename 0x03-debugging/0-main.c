635f62bd29991a4d5919c1b7aa500bedfb156575
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-23 19:19:44 -0700

    Debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - This should test for positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
