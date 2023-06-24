b17a63a562b35ee37c4d8be92ae6c8286ddb3a14
Alphonse-Inc <iammuyiwa@gmail.com>
2023-06-23 18:28:10 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,12 +7,11 @@
  */
 
 int _isdigit(int x)
-        
-{       
-        
-        if (x >= 48 && x <= 57)
-        {       
-                return (1);
-        }       
-        return (0);
+
+{
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
 }
