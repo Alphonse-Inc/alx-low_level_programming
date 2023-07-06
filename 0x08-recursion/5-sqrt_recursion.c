ecb3d702d55a33e32c8c81a5b57d171fb0226530
Alphonse-Inc <iammuyiwa@gmail.com>
2023-07-05 21:33:37 -0700

    Inception. Is it possible?

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,33 @@
+#include "main.h"
 
+/**
+ * _sqrt_recursion - This returns square root of a no.
+ * @n: number to calculate the square root of
+ * @i: iterator
+ * Return: the resulting square root
+ */
+
+int actual_sqrt_recursion(int n, int i);
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the square root of a no
+ * @n: number to calculate the square root
+ * @i: iterator
+ * Return: Always(success)
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int actual_prime(int n, int i);
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ * Return: 1 if it is a prime number and 0 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - This calculates if a prime number is prime
+ * @n: number to check
+ * @i: iterator
+ * Return: 1 if n is prime and 0 if otherwise
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
