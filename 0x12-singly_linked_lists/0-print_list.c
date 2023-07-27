6c1705f63861b3e7d7fc665f03376ec8ea34bade
Alphonse-Inc <iammuyiwa@gmail.com>
2023-07-27 14:53:03 -0700

    Print list task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @h: this is the pointer to list_t list
+ * Return: Always success 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
