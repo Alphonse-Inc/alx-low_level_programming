477e84b62607e07f639452b0d81e0aa63963629c
Alphonse-Inc <iammuyiwa@gmail.com>
2023-08-01 05:46:58 -0700

    Print list and List length

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,10 @@
 #include "lists.h"
-
 /**
- * print_listint – prints the elements of a linked list
+ * print_listint - prints the elements of a linked list
  * @h: linked list of type listint_t to print
- *
  * Return: the number of nodes
- *
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
